aebaf5e8ebbf0bae5dd3e4cdda5dc5c9c2440035
Gaurav Sharma <gaurav.work9456@gmail.com>
2023-10-15 13:22:35 +0530

    React Chapter 1

---
+++ index.js
@@ -0,0 +1,9 @@
+const heading=React.createElement("div",{id:"div"},[
+    React.createElement("h1",{},"Hello World from React head1 "),
+    React.createElement("h2",{},"Hello World from React head2 "),
+],[
+    React.createElement("h1",{},"Hello World from React head1 "),
+    React.createElement("h2",{},"Hello World from React head2 "),
+]);
+const root=ReactDOM.createRoot(document.getElementById('root'));
+root.render(heading)
